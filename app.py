8d4b0a57e4a4d0edd9c4c9e7875544e3442dc22a
Malcome Fein <mfein90@gmail.com>
2025-02-23 22:42:51 -0500

    feat: Add on_voice_state_update event to track user join/leave in voice channels

--- app.py
+++ app.py
@@ -17,5 +17,15 @@ async def on_ready():
     print(f"Logged in as: {bot.user.name}")
 
 
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
+    if not before.channel:
+        print(f'{member.name} joined {after.channel.name}')
+    if before.channel and not after.channel:
+        print("User left channel")
+
+
 if __name__ == '__main__':
     bot.run("PASTE YOUR TOKEN HERE")
